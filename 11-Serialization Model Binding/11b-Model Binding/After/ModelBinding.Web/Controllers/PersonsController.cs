1835973a5a54e4c6751592e5a731253e616d4b16
Anthony Sneed <tony@tonysneed.com>
2016-03-23 10:53:11 +0100

    Initial commit.

---
+++ 11-Serialization Model Binding/11b-Model Binding/After/ModelBinding.Web/Controllers/PersonsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using ModelBinding.Web.Models;
+
+namespace ModelBinding.Web.Controllers
+{
+    public class PersonsController : ApiController
+    {
+        private static readonly Dictionary<int, Person> Persons =
+            new Dictionary<int, Person>()
+        {
+            { 1, new Person { Name = "Peter", Age = 20} },
+            { 2, new Person { Name = "Paul", Age = 30} },
+            { 3, new Person { Name = "Mary", Age = 40} },
+        };
+
+        // GET: api/Person
+        public IEnumerable<Person> Get()
+        {
+            return Persons.Values;
+        }
+
+        // GET: api/Person/5
+        public Person Get(int id)
+        {
+            return Persons[1];
+        }
+
+        // POST: api/Person/5?Name=Peter&Age=20
+        public void Post(int id, [FromUri]Person value)
+        {
+            Persons.Add(id, value);
+        }
+
+        // PUT: api/Person/5??Name=Peter&Age=20
+        public void Put(int id, [FromUri]Person value)
+        {
+            Persons[id] = value;
+        }
+
+        // DELETE: api/Person/5
+        public void Delete(int id)
+        {
+            Persons.Remove(id);
+        }
+    }
+}
---
+++ 11-Serialization Model Binding/11b-Model Binding/After/ModelBinding.Web/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace ModelBinding.Web.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
